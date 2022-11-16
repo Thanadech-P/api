a03eed6e4ae5467186447d932c57af42ee46fde5
woranunPee <woranun.dev@gmail.com>
2022-11-17 00:29:55 +0700

    change package.json prisma version

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,10 @@ import { PurchaseModule } from './modules/purchase/purchase.module';
 import { StocksModule } from './modules/stocks/stocks.module';
 import { PartnerModule } from './modules/partner/partner.module';
 import { StockspareModule } from './modules/stockspare/stockspare.module';
+import { PrismaService } from 'prisma.service';
 @Module({
   imports: [AuthModule, UsersModule, PurchaseModule, StocksModule, PartnerModule, StockspareModule],
   controllers: [AppController],
-  providers: [AppService,],
+  providers: [AppService,PrismaService],
 })
 export class AppModule { }
